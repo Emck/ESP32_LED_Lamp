a1cc8eeec0be1616d9982d86de4879b6a9081729
Emck <emck.do@gmail.com>
2021-06-02 14:15:46 +0800

    add touch button by GPIO4

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,43 @@
 #include <Arduino.h>
 
-#define LED_LAMP 2  // GPIO 2
+#define LED_LAMP 2    // ESP32 GPIO 2
+#define TOUTCH_PIN 4  // ESP32 GPIO 4
 
-void setup()
-{
-  Serial.begin(9600);
-  // initialize LED digital pin as an output.
-  pinMode(LED_LAMP, OUTPUT);
+int touch_flag = 0;
+
+void setup() {
+    Serial.begin(9600);
+    // initialize LED digital pin as an output.
+    pinMode(LED_LAMP, OUTPUT);
+    // initialize LED LAMP Off.
+    digitalWrite(LED_LAMP, LOW);
+
+    Serial.println("Init Finish");
 }
 
-void loop()
-{
-  Serial.println("turn the LED on");
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_LAMP, HIGH);
-  // wait for 2 second
-  delay(2000);
+void loop() {
+    /*
+    Serial.println("turn the LED On");
+    digitalWrite(LED_LAMP, HIGH); // turn the LED On (HIGH is the voltage level)
+    delay(2000);                  // wait for 2 second
 
-  Serial.println("turn the LED off");
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_LAMP, LOW);
-   // wait for 1 second
-  delay(1000);
+    Serial.println("turn the LED Off");
+    digitalWrite(LED_LAMP, LOW); // turn the LED Off by making the voltage LOW
+    delay(2000);                 // wait for 2 second
+    */
+
+    if (touchRead(TOUTCH_PIN) > 40) {
+        if (touch_flag != LOW) {
+            Serial.println("turn the LED Off");
+        }
+        touch_flag = LOW;
+    } else {
+        if (touch_flag != HIGH) {
+            Serial.println("turn the LED On");
+        }
+        touch_flag = HIGH;
+    }
+    digitalWrite(LED_LAMP, touch_flag);
+
+    delay(50);
 }
